7ebb8d25ba2e7f4d701bef3891b67ab4a1b49f6f
Alvaro Pimentel <alvarohcpimentel@gmail.com>
2023-12-07 19:47:18 -0300

    Created the initialization service for Farmacia.

--- src/main/java/com/devinhouse/DEVinPharmacy/controller/InicializacaoController.java
+++ src/main/java/com/devinhouse/DEVinPharmacy/controller/InicializacaoController.java
@@ -1,9 +1,9 @@
 package com.devinhouse.DEVinPharmacy.controller;
 
 import com.devinhouse.DEVinPharmacy.data.TipoMedicamento;
-import com.devinhouse.DEVinPharmacy.model.Endereco;
 import com.devinhouse.DEVinPharmacy.model.Farmacia;
 import com.devinhouse.DEVinPharmacy.model.Medicamento;
+import com.devinhouse.DEVinPharmacy.service.FarmaciaInicializacaoService;
 import com.devinhouse.DEVinPharmacy.service.FarmaciaRepositoryService;
 import com.devinhouse.DEVinPharmacy.service.MedicamentoRepositoryService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,13 +22,15 @@ public class InicializacaoController {
     @Autowired
     FarmaciaRepositoryService farmaciaService;
     @Autowired
+    FarmaciaInicializacaoService farmaciaInicializacaoService;
+    @Autowired
     MedicamentoRepositoryService medicamentoService;
 
     @PostMapping
     public ResponseEntity<HttpStatus> inicialize() {
 
         if (farmaciaService.GetAll().isEmpty()) {
-            List<Farmacia> farmacias = this.farmaciaInicializacao();
+            List<Farmacia> farmacias = farmaciaInicializacaoService.inicializarDados();
             farmaciaService.SaveAll(farmacias);
         }
 
@@ -40,49 +42,6 @@ public class InicializacaoController {
         return ResponseEntity.ok().build();
     };
 
-
-    private List<Farmacia> farmaciaInicializacao(){
-        Farmacia farmacia1 = new Farmacia(
-                90561736000121L,
-                "DevMed Ltda",
-                "Farmácia DevMed",
-                "devmed@farmacia.com",
-                "(44)4444-4444",
-                "(44)9444-4441",
-                new Endereco(
-                        88888999L,
-                        "Rua Porto Real",
-                        67,
-                        "",
-                        "Westeros",
-                        "Berlim",
-                        "SC",
-                        15.23456,
-                        2.8678687
-                )
-        );
-
-        Farmacia farmacia2 = new Farmacia(
-                43178995000198L,
-                "MedHouse Ltda",
-                "Farmácia MedHouse",
-                "medhouse@farmacia.com",
-                "(55)5555-5555",
-                "(45)95555-5555",
-                new Endereco(
-                        8877799L,
-                        "Rua Madrid",
-                        76,
-                        "",
-                        "Winterfell",
-                        "Estocolmo",
-                        "SC",
-                        19.254356,
-                        3.8987687
-                )
-        );
-        return List.of(farmacia1,farmacia2);
-    }
 
     private List<Medicamento> medicamentoInicializacao(){
         Medicamento medicamento1 = new Medicamento(
---
+++ src/main/java/com/devinhouse/DEVinPharmacy/service/FarmaciaInicializacaoService.java
@@ -0,0 +1,54 @@
+package com.devinhouse.DEVinPharmacy.service;
+
+import com.devinhouse.DEVinPharmacy.model.Endereco;
+import com.devinhouse.DEVinPharmacy.model.Farmacia;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FarmaciaInicializacaoService {
+
+    public List<Farmacia> inicializarDados(){
+        Farmacia farmacia1 = new Farmacia(
+                90561736000121L,
+                "DevMed Ltda",
+                "Farmácia DevMed",
+                "devmed@farmacia.com",
+                "(44)4444-4444",
+                "(44)9444-4441",
+                new Endereco(
+                        88888999L,
+                        "Rua Porto Real",
+                        67,
+                        "",
+                        "Westeros",
+                        "Berlim",
+                        "SC",
+                        15.23456,
+                        2.8678687
+                )
+        );
+
+        Farmacia farmacia2 = new Farmacia(
+                43178995000198L,
+                "MedHouse Ltda",
+                "Farmácia MedHouse",
+                "medhouse@farmacia.com",
+                "(55)5555-5555",
+                "(45)95555-5555",
+                new Endereco(
+                        8877799L,
+                        "Rua Madrid",
+                        76,
+                        "",
+                        "Winterfell",
+                        "Estocolmo",
+                        "SC",
+                        19.254356,
+                        3.8987687
+                )
+        );
+        return List.of(farmacia1,farmacia2);
+    };
+}
