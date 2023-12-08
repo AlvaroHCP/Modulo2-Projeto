7e5743589b327b0acdd7969d5442e15df4a1f057
Alvaro Pimentel <alvarohcpimentel@gmail.com>
2023-12-07 22:30:08 -0300

    Get method for Farmacias is done.

--- src/main/java/com/devinhouse/DEVinPharmacy/connection/MyHttpResponse.java
+++ src/main/java/com/devinhouse/DEVinPharmacy/connection/MyHttpResponse.java
@@ -1,14 +1,49 @@
 package com.devinhouse.DEVinPharmacy.connection;
 
+import com.devinhouse.DEVinPharmacy.model.Estoque;
+import com.devinhouse.DEVinPharmacy.model.Farmacia;
+import com.devinhouse.DEVinPharmacy.model.Medicamento;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.util.List;
+
 public class MyHttpResponse {
-    private static Object body;
+
     public static ResponseEntity<HttpStatus> ok(){
         return ResponseEntity.ok().build();
     };
-    public static ResponseEntity<?> ok(Object body){
+    public static ResponseEntity<HttpStatus> badRequest(){
+        return ResponseEntity.badRequest().build();
+    };
+    public static ResponseEntity<?> badRequestBody(Object message) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(message);
+    };
+    public static ResponseEntity<HttpStatus> noContent(){
+        return ResponseEntity.noContent().build();
+    };
+    public static ResponseEntity<HttpStatus> notFound(){
+        return ResponseEntity.notFound().build();
+    };
+    public static ResponseEntity<Object> Ok(Object body){
+        return ResponseEntity.ok(body);
+    };
+    public static ResponseEntity<Farmacia> farmaciaOk(Farmacia body){
+        return ResponseEntity.ok(body);
+    };
+    public static ResponseEntity<Medicamento> medicamentoOk(Medicamento body){
+        return ResponseEntity.ok(body);
+    };
+    public static ResponseEntity<Estoque> estoqueOk(Estoque body){
+        return ResponseEntity.ok(body);
+    };
+    public static ResponseEntity<List<Farmacia>> farmaciasOk(List<Farmacia> body){
+        return ResponseEntity.ok(body);
+    };
+    public static ResponseEntity<List<Medicamento>> medicamentosOk(List<Medicamento> body){
+        return ResponseEntity.ok(body);
+    };
+    public static ResponseEntity<List<Estoque>> estoquesOk(List<Estoque> body){
         return ResponseEntity.ok(body);
     };
 }
---
+++ src/main/java/com/devinhouse/DEVinPharmacy/controller/FarmaciaController.java
@@ -0,0 +1,24 @@
+package com.devinhouse.DEVinPharmacy.controller;
+
+import com.devinhouse.DEVinPharmacy.connection.MyHttpResponse;
+import com.devinhouse.DEVinPharmacy.model.Farmacia;
+import com.devinhouse.DEVinPharmacy.service.FarmaciaRepositoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/farmacias")
+public class FarmaciaController {
+    @Autowired
+    FarmaciaRepositoryService farmaciaRepoService;
+    @GetMapping
+    public ResponseEntity<List<Farmacia>> farmaciasGet(){
+        List<Farmacia> farmacias = farmaciaRepoService.GetAll();
+        return MyHttpResponse.farmaciasOk(farmacias);
+    };
+}
