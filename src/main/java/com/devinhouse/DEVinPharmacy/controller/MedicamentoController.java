56c8423dde65c51708d99561ac08b6be1f6f52a3
Alvaro Pimentel <alvarohcpimentel@gmail.com>
2023-12-09 11:22:48 -0300

    Endpoint /medicamentos with a GET request is working properly.

--- src/main/java/com/devinhouse/DEVinPharmacy/controller/MedicamentoController.java
+++ src/main/java/com/devinhouse/DEVinPharmacy/controller/MedicamentoController.java
@@ -1,17 +1,31 @@
 package com.devinhouse.DEVinPharmacy.controller;
 
 import com.devinhouse.DEVinPharmacy.dto.MedicamentoResponse;
+import com.devinhouse.DEVinPharmacy.model.Medicamento;
+import com.devinhouse.DEVinPharmacy.service.MedicamentoRepositoryService;
+import org.apache.catalina.mapper.Mapper;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/medicamentos")
 public class MedicamentoController {
+    @Autowired
+    MedicamentoRepositoryService medicamentoRepoService;
+    @Autowired
+    ModelMapper mapper;
     @GetMapping
-    public ResponseEntity<MedicamentoResponse> medicamentosGet(){
-
-        return ResponseEntity.ok(new MedicamentoResponse());
+    public ResponseEntity<List<MedicamentoResponse>> medicamentosGet(){
+        List<Medicamento> medicamentos = medicamentoRepoService.GetAll();
+        List<MedicamentoResponse> response = medicamentos.stream().map(item ->
+                mapper.map(item, MedicamentoResponse.class)
+        ).toList();
+        return ResponseEntity.ok(response);
     };
 }
