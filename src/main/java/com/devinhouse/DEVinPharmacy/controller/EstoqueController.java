42c23d0da472442e518fc9a20a9282e905bf51a4
Alvaro Pimentel <alvarohcpimentel@gmail.com>
2023-12-09 13:55:34 -0300

    Created the Estoque RestController, and the endpoints /estoque and /estoque/{cnpj} .

---
+++ src/main/java/com/devinhouse/DEVinPharmacy/controller/EstoqueController.java
@@ -0,0 +1,24 @@
+package com.devinhouse.DEVinPharmacy.controller;
+
+import com.devinhouse.DEVinPharmacy.service.EstoqueRepositoryService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/estoque")
+public class EstoqueController {
+    @Autowired
+    EstoqueRepositoryService estoqueRepoService;
+    @Autowired
+    ModelMapper mapper;
+
+    @GetMapping("{cnpj}")
+    public ResponseEntity<?> estoqueGet(@PathVariable Long cnpj){
+        return ResponseEntity.ok(cnpj);
+    };
+}
